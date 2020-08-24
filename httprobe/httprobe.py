7e3411206ca53248677479f1c722c093d3d0360f
markus <markus.luik@nortal.com>
2020-08-24 22:08:07 +0300

    do not generate domains-all when one exists

--- httprobe/httprobe.py
+++ httprobe/httprobe.py
@@ -7,7 +7,7 @@ class HttProbe:
     def __init__(self, working_dir):
         self.working_dir = working_dir
         self.results = '{}/domains-probed'.format(working_dir)
-        self.unique_domains = '{}/domains-all'.format(working_dir)
+        self.all_domains = '{}/domains-all'.format(working_dir)
         self.blacklist_file = './httprobe/blacklist'
 
     def run_all(self):
@@ -19,21 +19,25 @@ class HttProbe:
         print('[+] {}'.format(msg))
 
     def run_httprobe(self):
-        if self.has_httprobe_run_today(self.results):
+        if self.does_file_exist(self.results):
             self.log('Skipping httprobe')
             return
 
-        self.find_unique_domains()
+        self.find_all_domains()
         self.httprobe()
 
-    def has_httprobe_run_today(self, scan_results):
-        domain_file = Path(scan_results)
-        return domain_file.is_file()
+    def does_file_exist(self, file_path):
+        file = Path(file_path)
+        return file.is_file()
 
-    def find_unique_domains(self):
+    def find_all_domains(self):
+        if self.does_file_exist(self.all_domains):
+            self.log('Skipping domains-all file generation')
+            return
+
         targets = self.get_probe_targets_files()
 
-        with open(self.unique_domains, "w") as outfile:
+        with open(self.all_domains, "w") as outfile:
             cat = subprocess.Popen(['awk', '1'] + targets, stdout=subprocess.PIPE)
             grep_blacklist = subprocess.Popen(['grep', '-v', '-f', self.blacklist_file], stdin=cat.stdout, stdout=subprocess.PIPE)
             sort = subprocess.Popen(['sort', '-u'], stdin=grep_blacklist.stdout, stdout=outfile)
@@ -47,7 +51,7 @@ class HttProbe:
 
     def httprobe(self):
         with open(self.results, "w") as outfile:
-            cat = subprocess.Popen(['cat', self.unique_domains], stdout=subprocess.PIPE)
+            cat = subprocess.Popen(['cat', self.all_domains], stdout=subprocess.PIPE)
             probe = subprocess.Popen(['httprobe'], stdin=cat.stdout, stdout=subprocess.PIPE)
             sed_0 = subprocess.Popen(['sed', 's/http:\/\///g'], stdin=probe.stdout, stdout=subprocess.PIPE)
             sed_1 = subprocess.Popen(['sed', 's/https:\/\///g'], stdin=sed_0.stdout, stdout=subprocess.PIPE)
